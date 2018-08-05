351e73303a1026e52e61cfe721cb9a514d02c351
Fichufish <39399950+Fichufish@users.noreply.github.com>
2018-08-05 20:50:45 +0800

    first commit

---
+++ templates/_footer.php
@@ -0,0 +1,4 @@
+<footer>
+	<p>© Copyright Fichufish 2018</p>
+	<p>最後更新: 2018.08.05</p>
+</footer>
---
+++ templates/_nav.php
@@ -0,0 +1,7 @@
+<nav>
+	<ul>
+		<li><a href="index.php">簡介</a></li>
+		<li><a href="findroommate.php">找室友</a></li>
+		<li><a href="plusinfo.php">登錄資料</a></li>
+	</ul>
+</nav>
